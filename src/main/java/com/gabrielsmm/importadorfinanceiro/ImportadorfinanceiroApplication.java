71f0302e5324a9586e8062b1768a4996fe372019
gabrielsmm <gabrielsmm251@gmail.com>
2025-04-25 21:16:05 -0300

    Criação do projeto

---
+++ src/main/java/com/gabrielsmm/importadorfinanceiro/ImportadorfinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsmm.importadorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportadorfinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportadorfinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielsmm/importadorfinanceiro/ImportadorfinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsmm.importadorfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportadorfinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
